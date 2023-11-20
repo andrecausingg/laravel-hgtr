154cd367de6e000ed85cc9060e884539cab35516
Andre <95618374+andrecausingg@users.noreply.github.com>
2023-11-20 10:16:26 +0800

    reqyuest update 240 per 30 seconsds

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,9 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            // ADD
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':240,30',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
