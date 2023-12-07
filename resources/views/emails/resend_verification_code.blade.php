a4398a16013f74ffc75beb8b5066b7e47f165987
andrecausingg <95618374+andrecausingg@users.noreply.github.com>
2023-12-07 10:09:58 +0800

    resend code and very code ui email

--- resources/views/emails/resend_verification_code.blade.php
+++ resources/views/emails/resend_verification_code.blade.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hgtr - New Code</title>
 </head>
+
 <body>
-    <div style="font-family: 'Segoe UI', sans-serif; width: 500px; margin: auto; padding: 40px; box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1); border: 1px solid #ECEFF3; border-radius: 12px; text-align: center;">
-        <h2 style="font-size: 24px; margin-bottom: 16px;">Dear valued {{ $name }},</h2>
+    <div
+        style="font-family: 'Segoe UI', sans-serif; width: 500px; margin: auto; padding: 40px; box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1); border: 1px solid #ECEFF3; border-radius: 12px; text-align: center;">
+        <img src="http://127.0.0.1:5173/src/assets/images/img/HGTR%20LOGO%203.png" alt="Logo" style="width:150px;">
         <p style="font-weight: 400; font-size: 16px; margin: 12px 0;">Here is your new verification code:</p>
-        <p style="font-size: 32px; font-weight: 600; color: #007bff; margin: 0;">{{ $verificationCode }}</p>
-        <p style="font-weight: 400; font-size: 16px; margin: 12px 0;">If you did not request this verification code, please ignore this message.</p>
+        <p style="font-size: 32px; font-weight: 600; margin: 0;">{{ $verificationCode }}</p>
+        <p style="font-weight: 400; font-size: 16px; margin: 12px 0;">If you did not request this verification code,
+            please ignore this message.</p>
     </div>
 </body>
+
 </html>
--- resources/views/emails/verification_mail.blade.php
+++ resources/views/emails/verification_mail.blade.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hgtr - Verification Code</title>
 </head>
+
 <body>
-    <div style="font-family: Segoe UI; width: 500px; margin: auto auto; padding: 56px; box-shadow: 0 4px 4px 0 rgba(233, 240, 243, 0.4); border: 1px solid #ECEFF3; border-radius: 12px;">
-        <h2 style="font-size: 30px;">Dear valued {{ $name }},</h2>
-        <p style="font-weight: 400; font-size: 16px;">Thank you for signing up for our service. To complete the verification process, please use the following code: <span style="font-size: 24px; font-weight: 600;">{{ $verificationCode }}</span>.</p>
-        <p style="font-weight: 400; font-size: 16px;">If you did not request this verification code, please ignore this message.</p>
+    <div
+        style="text-align: center; font-family: Segoe UI; width: 500px; margin: auto auto; padding: 56px; box-shadow: 0 4px 4px 0 rgba(233, 240, 243, 0.4); border: 1px solid #ECEFF3; border-radius: 12px;">
+        <div style="text-align: center;">
+            <img src="http://127.0.0.1:5173/src/assets/images/img/HGTR%20LOGO%203.png" alt="Logo"
+                style="width:150px; margin-left:auto; margin-right:auto;">
+        </div>
+        <p style="font-weight: 400; font-size: 16px;">Thank you for signing up for our service. To complete the
+            verification process, please use the following code: </p>
+            <span style="font-size: 32px; font-weight: 600; margin: 0;">{{
+                $verificationCode }}</span>
+        <p style="font-weight: 400; font-size: 16px;">If you did not request this verification code, please ignore this
+            message.</p>
     </div>
 </body>
+
 </html>
