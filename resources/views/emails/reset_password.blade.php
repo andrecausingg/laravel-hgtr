f80bcfd930fcb91ae7960935437772456de6b72e
andrecausingg <95618374+andrecausingg@users.noreply.github.com>
2023-12-11 09:26:22 +0800

    .

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -8,7 +8,7 @@
         <h1>Reset Password</h1>
         <p style="font-weight: 400; font-size: 16px;">If you've lost your password or wish to reset it, use the link below to get started. 
             You'll receive an email containing a verification key that you'll need to enter during the password reset process to ensure the security of your account.</p>
-        <a href="http://localhost:5173/update-password?verificationKey={{ $verificationToken }}&email={{ $email }}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: white; border: none; border-radius: 5px; text-decoration: none; font-size: 16px; margin-top: 20px;">Reset Password</a>
+        <a href="https://hugoteroclothing.com/update-password?verificationKey={{ $verificationToken }}&email={{ $email }}" style="display: inline-block; padding: 10px 20px; background-color: #007BFF; color: white; border: none; border-radius: 5px; text-decoration: none; font-size: 16px; margin-top: 20px;">Reset Password</a>
         <p style="font-weight: 400; font-size: 16px; margin-top: 20px;">If you did not request this verification code, please ignore this message.</p>
     </div>
 </body>
